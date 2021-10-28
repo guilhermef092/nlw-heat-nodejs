dfe2450478ce07560e849bc6db6be7ed80d08d37
Guilherme Figueiredo <guilherme.figueiredo092@gmail.com>
2021-10-28 09:12:21 -0300

    Project NLW Heat - Node.js

---
+++ src/services/GetLast3MessagesService.ts
@@ -0,0 +1,20 @@
+import prismaCliente from '../prisma'
+
+class GetLast3MessagesService {
+    async execute() {
+        const messages = await prismaCliente.message.findMany({
+            take: 3,
+            orderBy: {
+                create_at: 'desc'
+            },
+            include: {
+                user: true
+            },
+
+        })
+
+        return messages
+    }
+}
+
+export { GetLast3MessagesService }
